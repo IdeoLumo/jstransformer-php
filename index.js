a1a251402ecf9e574197613c50dbb156f85ca038
Jikstra <jikstra@disroot.org>
2018-02-15 17:52:33 +0100

    improved rendered php to allow php block to have html in it

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ exports.name = 'php'
 exports.inputFormats = ['php']
 exports.outputFormat = 'php-html'
 
+const PHP_START = '<?php'
+const PHP_END = '?>'
+
 exports.render = function (str) {
-  if (str.indexOf('\n') === -1) {
-    return '<?php ' + str + ' ?>'
-  }
-  return '\n<?php\n' + str + '\n?>'
+  const addStartTag = str.indexOf(PHP_START) === -1
+  const addEndTag = str.indexOf(PHP_END) === -1
+  const singleLine = str.indexOf('\n') === -1
+
+  return (addStartTag ? PHP_START : '') +
+         (singleLine ? ' ' : '\n') +
+         str +
+         (singleLine ? ' ' : '\n') +
+         (addEndTag ? PHP_END : '')
 }
